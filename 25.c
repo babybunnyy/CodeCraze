b4a4076733230407fcb6275623e4251cc724d727
goddess <secretgoddess6666@gmail.com>
2023-12-13 19:47:08 +0100

    compute space

--- 25.c
+++ 25.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// this program computes the total number of bytes required based on user-input data. 
 
 int main(void){
-	int n;
+	int n; // number of inputs
 	scanf("%d",&n);
-	int num[100];
-	char type[100];
+	int i;
+	int length[101];
+	char type[101];
+	bool valid = true;
 	int bytes = 0;
-	for(int i=0;i<n;i++){
-		scanf("%d %c",&num[i],&type[i]);
-		if(type[i] == 'c'){
-			bytes += num[i];
-		}else if(type[i] == 'i'){
-			bytes += (num[i] * 4);
+	for(i=0;i<n;i++){
+		scanf("%d %c",&length[i],&type[i]);
+		if(type[i] == 'i'){
+			bytes += length[i] * sizeof(int);
 		}else if(type[i] == 'd'){
-			bytes += (num[i] * 8);
-		}	
-	}
-	for(int i=0;i<n;i++){
-		if(type[i] == 'c' || type[i] == 'd' || type[i] == 'i'){
-			printf("%d bytes",bytes);
-		}else {
-			printf("Invalid tracking code type");
+			bytes += length[i] * sizeof(double);
+		}else if(type[i] == 'c'){
+			bytes += length[i] * sizeof(char);
+		}else{
+			valid = false;
 		}
 	}
+	if(valid){
+		printf("%d\n",bytes);
+	}else{
+		printf("Invalid tracking code type\n");
+	}
+	return 0;
 }
+
