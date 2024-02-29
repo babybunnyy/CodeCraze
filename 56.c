fdd8046fd3ff7df87a14d4e694832ef6be568489
goddess <secretgoddess6666@gmail.com>
2024-02-29 16:06:04 +0100

    triangles

---
+++ 56.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for(int i = 4; i > 0; i--)
+	{
+		for(int j = 0; j < i; j++)
+			printf("*");
+		for(int j = i; j < 4; j++)
+			printf(" ");
+		for(int j = i; j < 4; j++)
+			printf(" ");
+		for(int j = 0; j < i; j++)
+			printf("*");
+		printf("\n");
+	}
+	
+	for(int i = 4; i > 0; i--)
+	{
+		for(int j = i-1; j < 4; j++)
+			printf("*");
+		for(int j = 1; j < i; j++)
+			printf(" ");
+		for(int j = 1; j < i; j++)
+			printf(" ");
+		for(int j = i-1; j < 4; j++)
+			printf("*");
+		printf("\n");
+	}
+}
+
+		
